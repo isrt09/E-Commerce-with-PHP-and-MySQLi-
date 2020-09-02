d6107039b833fe0a1318785e7168029a1b86567e
Mazedur <mazedur@gmail.com>
2020-09-02 16:20:31 +0600

    Layout Design setup

--- index.php
+++ index.php
@@ -17,24 +17,28 @@
 		<!-- Header Ends -->
 		<!-- Navbar starts -->
 		<div class="navbar_wrapper">
-			<!-- Left Sidebar Start -->
-			<div class="left_sidebar">
-				LEFT SIDEBAR
-			</div>
-			<!-- Left Sidebar End -->
-			<!-- Right Content -->
-			<div class="right_content">
-				RIGHT CONTENT
-			</div>
-			<!-- Right Content End -->
+			<?php echo "NAVBAR"; ?>
 		</div>
 		<!-- Navbar Ends -->
 		<!-- Main Content Starts -->
 		<div class="content_wrapper">
-			Content Section
+			<!-- Left Sidebar Start -->
+			<div class="left_sidebar">
+				<?php echo "LEFT SIDEBAR"; ?>
+			</div>
+			<!-- Left Sidebar End -->
+			<!-- Right Content -->
+			<div class="right_content">
+				<?php echo "RIGHT CONTENT"; ?>
+			</div>
+			<!-- Right Content End -->
 		</div>
 		<!-- Main Content Ends -->
-		<div>Footer Section</div>
+		<!-- Footer Start -->
+		<div class="footer_wrapper">
+			<?php echo "FOOTER"; ?>
+		</div>
+		<!-- Footer End -->
 	</div>
 	<!-- End Main Container -->
 </body>
