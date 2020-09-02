78187c5b8a9e792b3daca03625a1569734908226
Mazedur <mazedur@gmail.com>
2020-09-02 16:17:04 +0600

    Layout Design setup

--- index.php
+++ index.php
@@ -4,8 +4,38 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>E-Commmerce Solution</title>
+	<link rel="stylesheet" href="styles/style.css" media="all">
 </head>
 <body>
-	<h1>E-Commerce Shop</h1>
+	<!-- Main Container -->
+	<div class="main_wrapper">
+		<!-- Header Starts -->
+		<div class="header_wrapper">			
+			<img src="images/logo.gif" alt="" style="float: left;" >			
+			<img src="images/banner.jpg" alt="" style="float: right;" >						
+		</div>		
+		<!-- Header Ends -->
+		<!-- Navbar starts -->
+		<div class="navbar_wrapper">
+			<!-- Left Sidebar Start -->
+			<div class="left_sidebar">
+				LEFT SIDEBAR
+			</div>
+			<!-- Left Sidebar End -->
+			<!-- Right Content -->
+			<div class="right_content">
+				RIGHT CONTENT
+			</div>
+			<!-- Right Content End -->
+		</div>
+		<!-- Navbar Ends -->
+		<!-- Main Content Starts -->
+		<div class="content_wrapper">
+			Content Section
+		</div>
+		<!-- Main Content Ends -->
+		<div>Footer Section</div>
+	</div>
+	<!-- End Main Container -->
 </body>
 </html>
