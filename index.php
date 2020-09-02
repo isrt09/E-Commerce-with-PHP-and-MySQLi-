4f801d6e2de059f508c8b4817183592d20389fff
Mazedur <mazedur@gmail.com>
2020-09-02 14:49:04 +0600

    Project Setup file & folders

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>E-Commmerce Solution</title>
+</head>
+<body>
+	<h1>E-Commerce Shop</h1>
+</body>
+</html>
